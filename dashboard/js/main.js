c31a98a2e56238e524d32ae5786db3329ac029dc
Augustin-SysFera <augustin@sysfera.com>
2014-03-27 18:57:35 +0100

    Adds the dashboard and a table for the tickets, with the attached logic.

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,1 +1,30 @@
+var clock = document.getElementById("countdown")  
+  , targetDate = new Date(2014, 03, 11);
 
+clock.innerHTML = countdown(targetDate, null, countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS, 3).toString();
+setInterval(function(){  
+  clock.innerHTML = countdown(targetDate, null, countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS, 3).toString();
+}, 1000);
+
+
+var templateStats;
+
+$.get('templates/projects.mustache.html', function(template) {
+  templateStats = $(template).filter('#template-stats').html();
+});
+
+setInterval(function(){
+  $.getJSON('data/data.json', function(data) {
+    data["projects"].forEach(function(it){
+      if (it.deadline < 24) {
+        it.deadlineStatus = "status"
+      } else if (it.deadline < 48) {
+        it.deadlineStatus = "warning"
+      } else {
+        it.deadlineStatus = "danger"
+      }
+      it.deadline = it.deadline.toLocaleString()
+    });
+    $('#stats').html(Mustache.render(templateStats, data))
+  });
+}, 1000);
