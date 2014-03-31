dcc0328c2ff89a930d35544875fe29e37deced15
Augustin-SysFera <augustin@sysfera.com>
2014-03-31 18:15:40 +0200

    Better handling of the date objects.

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -6,7 +6,7 @@ var clock = $("#countdown")
   , statsEquipe = $('#statsEquipe')
   , jenkinsBuilds = $('#jenkinsBuilds')
   , github = $('#github')
-  , nextSprint = new Date(2014, 03, 11, 18)
+  , nextSprint
   , refreshTime = 10000
   , templates, templateStatsSysFera, templateStatsClient, templateStatsEquipe, templateJenkins, templateGithub;
 
@@ -22,11 +22,22 @@ $.get('templates/templates.mustache.html', function(data) {
 
 // main refreshDisplay function
 function refreshDisplay() {
-  // Create and display the the countdown
-  sprintEnd = countdown(nextSprint, null, countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS, 3);
-  clock.html(sprintEnd.toString());
-  sprintDanger = (sprintEnd.value + 432000000) > 0
-  $("#sprintEnd").toggleClass( "sprintDanger", sprintDanger );
+
+  // Query the config file and create a display using the end date
+  $.getJSON('data/config.json', function(data) {
+    // nextSprint = eval(data['sprint']['date_end']);
+    nextSprint = new Date(
+      data['sprint']['end']['year'],
+      // months in JS are an index, so e.g., April is 3 and not 4
+      data['sprint']['end']['month'] - 1,
+      data['sprint']['end']['day'],
+      18, 0, 0);
+    sprintEnd = countdown(nextSprint, null, countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS, 3);
+    clock.html(sprintEnd.toString());
+    sprintDanger = (sprintEnd.value + 432000000) > 0
+    $("#sprintEnd").toggleClass( "sprintDanger", sprintDanger );
+  });
+
   // Query the project data JSON, render it against the Mustache template, and insert it in the project body tables
   $.getJSON('data/dataProjects.json', function(dataProjects) {
     dataProjects["projetsClient"].forEach(function(it){
@@ -42,6 +53,7 @@ function refreshDisplay() {
     statsClient.html(Mustache.render(templateStatsClient, dataProjects))
     statsSysFera.html(Mustache.render(templateStatsSysFera, dataProjects))
   });
+
   // Query the team data JSON, render it against the Mustache template, and insert it in the Team body table
   $.getJSON('data/dataTeam.json', function(dataTeam) {
     dataTeam["members"].forEach(function(member){
@@ -53,10 +65,12 @@ function refreshDisplay() {
     });
     statsEquipe.html(Mustache.render(templateStatsEquipe, dataTeam))
   });
+
   // Query the Jenkins data JSON, render it against the Mustache template, and insert it in the Jenkins builds table
   $.getJSON('data/dataJenkins.json', function(dataJenkins) {
     jenkinsBuilds.html(Mustache.render(templateJenkins, dataJenkins))
   });
+
   // Query the Changelogs JSON, render it against the Mustache template, and insert it in the Changelogs div  
   $.getJSON('data/dataGithub.json', function(dataGithub) {
     github.html(Mustache.render(templateGithub, dataGithub))
