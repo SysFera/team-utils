63e45f64aaeb02db7832ac1800357cd49b9cef8c
Augustin-SysFera <augustin@sysfera.com>
2014-04-01 09:56:20 +0200

    Adds Python scripts to the updateDashbord.sh.

--- dashboard/updateDashboard.sh
+++ dashboard/updateDashboard.sh
@@ -3,3 +3,11 @@
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_PATH
 git pull
+
+# Python scripts
+cd data
+python chiliTeam.py
+python chiliProjects.py
+python jenkinsBuilds.py
+python githubChangelogs.py
+cd -
