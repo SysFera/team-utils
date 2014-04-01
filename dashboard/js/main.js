d6170f01344fecdadb16b4d618785b0222b9f0a7
Augustin-SysFera <augustin@sysfera.com>
2014-04-01 11:22:55 +0200

    Additional fixes and improvements to the main.js file.

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -6,7 +6,6 @@ var clock = $("#countdown")
   , jenkinsBuilds = $('#jenkinsBuilds')
   , github = $('#github')
   , refreshTime = 10000
-  , nextSprint, sprintEnd, sprintDanger
   , templates, templateStatsSysFera, templateStatsClient, templateStatsEquipe, templateJenkins, templateGithub;
 
 // Initial templates gathering. Done only once.
@@ -24,9 +23,15 @@ function refreshDisplay() {
 
   // Query the config file and create a display using the end date
   $.getJSON('data/config.json', function(data) {
-    // nextSprint = eval(data['sprint']['date_end']);
-    refreshTime = data['global']['refreshtime'];
-    nextSprint = new Date(
+
+    // sets the page's refresh time
+    var refreshTime = data['global']['refreshtime'];
+    setInterval(function(){  
+      location.reload();
+    }, refreshTime);
+
+    // sets the date of the sprint's end
+    var nextSprint = new Date(
       data['sprint']['end']['year'],
       // months in JS are an index, so e.g., April is 3 and not 4
       data['sprint']['end']['month'] - 1,
@@ -34,7 +39,7 @@ function refreshDisplay() {
       18, 0, 0);
     sprintEnd = countdown(nextSprint, null, countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS, 3);
     clock.html(sprintEnd.toString());
-    sprintDanger = (sprintEnd.value + 432000000) > 0
+    var sprintDanger = (sprintEnd.value + 432000000) > 0
     $("#sprintEnd").toggleClass( "sprintDanger", sprintDanger );
   });
 
@@ -82,7 +87,3 @@ function refreshDisplay() {
 }
 
 refreshDisplay();
-
-setInterval(function(){  
-  location.reload();
-}, refreshTime);
