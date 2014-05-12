991a506106e348df10aadc138ccb91f00d3a0e83
Philippe Martinez <philippe.martinez@sysfera.com>
2014-05-12 17:31:17 +0200

    Add login & pass for demo

--- dashboard/data/PlatformChecking/checkDemo.sh
+++ dashboard/data/PlatformChecking/checkDemo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source ./libCheck.sh
 URL="http://54.246.101.26:8080"
+Wb_un="admin"
+Wb_pw="admin123"
+
 #WebBoard
 connectWebBoard "WebBoard" $URL
 checkAnyWebboardPage "WebBoard_project_list_page" $URL "project/list?lang=en" "<title>Project list"
