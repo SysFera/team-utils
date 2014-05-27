78eff8b95d09b3618b9006034163a3f615221558
Philippe Martinez <philippe.martinez@sysfera.com>
2014-05-27 11:44:56 +0200

    Delete TMS test : no script to test. Perhaps in the next version

--- checking/checkPlatform.sh
+++ checking/checkPlatform.sh
@@ -17,22 +17,8 @@ then
 fi
 
 source ${SYSFERA_DS_ROOT}/etc/check.conf
-#define username and password.
-Wb_un="admin"
-Wb_pw="admin123"
-Wb_URL="http://localhost:8080"
-ssh_user="ec2-user"
-ssh_machine="localhost"
-machineName="Machine1"
-machineId=1
 
-listdir_testpath="%2Fhome%2Fec2-user%2FSDS-backup%2Fdatabase%2F"
-simple_application_projectId=1
-simple_applicationId=1
-sample_completedJobId=22
-sample_containerJob=24
-
-pushd ${installation_path}/SDS/contrib/tests
+pushd ${SYSFERA_DS_ROOT}/contrib/tests
 
 source ./libCheck.sh
 
@@ -50,7 +36,6 @@ checkAnyWebboardPage "WebBoard_FMS:listDir" ${Wb_URL} "fileBrowser/getListDir?pw
 checkAnyWebboardPage "WebBoard_FMS:stat" ${Wb_URL} "fileBrowser/stat?file=${listdir_testpath}&machine=${machineName}" '"result":"success"'
 checkAnyWebboardPage "WebBoard_Work:getDoneRatio" ${Wb_URL} "work/getDoneRatio/${simple_application_projectId}?workId=${sample_completedJobId}" '{"doneRatio":100}'
 testSubmitWebBoardJob ${Wb_URL} "${simple_application_projectId}" "${simple_application_projectId}" "${machineId}"
-testSubmitCLIJob "~/.sds/vishnu.cfg" "vishnu_connect" "${installation_path}/SDS/contrib/tests" "vishnu_submit_job" ${machineName} "test.sh" "${sample_containerJob}" ${ssh_machine}
 #Add a global summary
 overallCheck
 
