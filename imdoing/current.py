7b10417b2efcdb386821ca7162a9a9ef2665cf93
Augustin-SysFera <augustin@sysfera.com>
2014-07-15 15:38:39 +0200

    t#919: imdoing current displays ticket trees.

--- imdoing/current.py
+++ imdoing/current.py
@@ -1,6 +1,15 @@
 # ~*~ coding: utf-8 ~*~
 
 
+class termColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+
 def get_issues(issues, attr, field, value):
     issues_id = []
     for i in issues:
@@ -11,12 +20,44 @@ def get_issues(issues, attr, field, value):
     return issues.filter(issues_id)
 
 
+def print_ticket(result, prefix=""):
+    if not result["parent_id"]:
+        start = termColors.OKBLUE
+        end = termColors.ENDC
+    else:
+        start = termColors.WARNING
+        end = termColors.ENDC
+
+    print start + prefix + u"#{id} = {project:^7} = OF: {of} = {assignee:^10} = {subject}".format(**result) + end
+    children = result['children']
+    if children:
+        for child in children:
+            new_prefix = "  " + prefix if prefix else "  \_ "
+            print_ticket(child, new_prefix)
+
+
+def print_tree(results):
+    if len(results) > 0:
+        for result in results:
+            print_ticket(result)
+    else:
+        print "No ticket found. Please check version_id is set correctly in config.json."
+
+
 def data(redmine, target, users):
     results = []
 
-    issues = get_issues(redmine.issue.filter(status_id='1'),
+    issues = get_issues(redmine.issue.filter(status_id='open'),
                         "fixed_version", "id", target)
+
     for issue in issues:
+        issue_id = issue['id']
+
+        if hasattr(issue, "parent"):
+            parent_id = issue.parent.id
+        else:
+            parent_id = None
+
         of = [cf['value'] for cf in issue['custom_fields']
               if cf['name'] == "OF"][0]
         if of == '':
@@ -31,21 +72,27 @@ def data(redmine, target, users):
             assignee = "unassigned"
 
         result = {
-            'number': issue['id'],
+            'id': issue_id,
             'subject': issue['subject'],
             'assignee': assignee,
             'of': of,
-            'project': issue['project']['name']
+            'project': issue['project']['name'],
+            'parent_id': parent_id,
+            'children': []
         }
         results.append(result)
+
+    for result in results:
+        for child in results:
+            if child['parent_id'] is not None:
+                if child['parent_id'] == result['id']:
+                    result['children'].append(child)
+
+    results = [x for x in results if not x['parent_id']]
 
     return results
 
 
 def run(rmine, target, users):
     tickets = data(rmine, target, users)
-    if len(tickets) > 0:
-        for ticket in tickets:
-            print u"#{number} = {project:^7} = OF: {of} = {assignee:^10} = {subject}".format(**ticket)
-    else:
-        print "No ticket found. Please check version_id is set correctly in config.json."
+    print_tree(tickets)
