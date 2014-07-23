83384e530120763cd978b11120ab9aa04819f246
Augustin-SysFera <augustin@sysfera.com>
2014-07-23 19:01:38 +0200

    Adds options and colors to imdoing current/list.

--- imdoing/current.py
+++ imdoing/current.py
@@ -1,4 +1,5 @@
 # ~*~ coding: utf-8 ~*~
+import argparse
 
 
 class termColors:
@@ -20,34 +21,68 @@ def get_issues(issues, attr, field, value):
     return issues.filter(issues_id)
 
 
-def print_ticket(result, prefix=""):
-    if not result["parent_id"]:
-        start = termColors.OKBLUE
-        end = termColors.ENDC
-    else:
-        start = termColors.WARNING
-        end = termColors.ENDC
+def legend():
+    string = "\n\nLegend: "
+    my_dict = {
+        "HEADER": "new",
+        "OKBLUE": "solved",
+        "OKGREEN": "closed",
+        "WARNING": "open",
+        "FAIL": "rejected"
+    }
 
-    print start + prefix + u"#{id} = {project:^7} = OF: {of} = {assignee:^10} = {subject}".format(**result) + end
-    children = result['children']
+    for k, v in my_dict.iteritems():
+        string += getattr(termColors, k) + v + termColors.ENDC + " - "
+
+    return string[:-3] + "\n"
+
+
+def color_status(ticket):
+    status = ticket['status']
+
+    if status == 1:
+        return termColors.HEADER
+    elif status == 2:
+        return termColors.WARNING
+    elif status == 3:
+        return termColors.OKBLUE
+    elif status == 5:
+        return termColors.OKGREEN
+    elif status == 6:
+        return termColors.FAIL
+
+
+def colorify(string, color):
+    return color + string + termColors.ENDC
+
+
+def print_ticket(ticket, prefix=""):
+    color = color_status(ticket)
+    string = prefix + u"#{id} = {project:^7} = OF: {of} = " \
+                      u"{assignee:^10} = {subject}".format(**ticket)
+    print colorify(string, color)
+    children = ticket['children']
     if children:
         for child in children:
             new_prefix = "  " + prefix if prefix else "  \_ "
             print_ticket(child, new_prefix)
 
 
-def print_tree(results):
-    if len(results) > 0:
-        for result in results:
+def print_tree(tickets):
+    if len(tickets) > 0:
+        print "\n"
+        for result in tickets:
             print_ticket(result)
+        print legend()
     else:
-        print "No ticket found. Please check version_id is set correctly in config.json."
+        print "No ticket found. Please check version_id " \
+              "is set correctly in config.json."
 
 
-def data(redmine, target, users):
+def data(redmine, target, users, status):
     results = []
 
-    issues = get_issues(redmine.issue.filter(status_id='open'),
+    issues = get_issues(redmine.issue.filter(status_id=status),
                         "fixed_version", "id", target)
 
     for issue in issues:
@@ -78,6 +113,7 @@ def data(redmine, target, users):
             'of': of,
             'project': issue['project']['name'],
             'parent_id': parent_id,
+            'status': issue['status']['id'],
             'children': []
         }
         results.append(result)
@@ -93,6 +129,19 @@ def data(redmine, target, users):
     return results
 
 
+def parse():
+    parser = argparse.ArgumentParser(
+        description='list tickets for the current sprint.')
+    parser.add_argument('current', type=str, help='the current sprint')
+    parser.add_argument('status', nargs='?', help='the command arguments',
+                        default='open', choices=['open', 'all', 'closed'])
+    args = parser.parse_args()
+    status = args.status
+
+    return "*" if status == "all" else status
+
+
 def run(rmine, target, users):
-    tickets = data(rmine, target, users)
+    args = parse()
+    tickets = data(rmine, target, users, args)
     print_tree(tickets)
