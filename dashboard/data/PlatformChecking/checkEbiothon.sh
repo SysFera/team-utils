15488469d24a93177668a45d1437582e26422db3
Philippe Martinez <philippe.martinez@sysfera.com>
2014-09-22 14:15:13 +0200

    Check ebioton :re-enable check fms stat

--- dashboard/data/PlatformChecking/checkEbiothon.sh
+++ dashboard/data/PlatformChecking/checkEbiothon.sh
@@ -18,7 +18,7 @@ checkAnyWebboardPage "WebBoard_project_list_page" ${Wb_URL} "project/list?lang=e
 checkAnyWebboardPage "WebBoard_file_manager_page" ${Wb_URL} "fileBrowser/index?lang=en" "<title>File Manager - SysFera-DS"
 checkAnyWebboardPage "WebBoard_FMS:getUserHome" ${Wb_URL} "fileBrowser/getUserHome?machineId=babel" '"result":"success"'
 checkAnyWebboardPage "WebBoard_FMS:listDir" ${Wb_URL} "fileBrowser/getListDir?pwd=%2Fworkgpfs%2Fidris%2Febiothon%2Fvishnu1&machine=babel&offset=0&max=20&sort=name&order=asc&noHidden=true" '"result":"success"'
-#checkAnyWebboardPage "WebBoard_FMS:stat" ${Wb_URL} "fileBrowser/stat?file=%2Fworkgpfs%2Fidris%2Febiothon/vishnu1&machine=babel" '"result":"success"'
+checkAnyWebboardPage "WebBoard_FMS:stat" ${Wb_URL} "fileBrowser/stat?file=%2Fworkgpfs%2Fidris%2Febiothon/vishnu1&machine=babel" '"result":"success"'
 checkAnyWebboardPage "WebBoard_Work:getDoneRatio" ${Wb_URL} "work/getDoneRatio/3?workId=95" '{"doneRatio":100}'
 #testSubmitWebBoardJob ${Wb_URL} "3" "3" "babel"
 testSubmitCLIJob "/home/vishnu/sysfera-ds/etc/vishnu.server.cfg" "vishnu_connect" "/home/vishnu/applis/scripts" "vishnu_submit_job" "babel" "test.sh" "200" "ebiothon-vm"
